31da9fe913a51623c00524ca25a7d5961e95aa7f
pducos <pducos@student.42.fr>
2022-08-19 19:01:30 +0200

    script.sh: add error if less than 2 args

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Error: Usage" > /dev/stderr
+	echo "Usage: sh ./sc.sh <binary.macho> <function>" > /dev/stderr
+    exit 1
+fi
+
+PROG=$1
+FUNC=$2
+
+objdump -d $PROG |
+awk -v RS= "/^[[:xdigit:]]+ <$FUNC>/" | 
+tail -n +2 |
+awk '{printf "\"\\x%s\\x%s\\x%s\\x%s\" // %s%5s %s\n", $2,$3,$4,$5,$6,$7,$8}'
