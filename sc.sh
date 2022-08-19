6cd2b2258409d5a381ba4e84879dc2393f2b7bef
pducos <pducos@student.42.fr>
2022-08-19 18:55:37 +0200

    add cmd line args to script

--- sc.sh
+++ sc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-PROG="bin.macho"
 
-av.str.1
+if [ $# -eq 0 ]; then
+    echo "Error: Usage" > /dev/stderr
+	echo "Usage: sh ./sc.sh <binary.macho> <function>" > /dev/stderr
+    exit 1
+fi
+
+PROG=$1
+FUNC=$2
 
 objdump -d $PROG |
-awk -v RS= "/^[[:xdigit:]]+ <_main>/" | 
+awk -v RS= "/^[[:xdigit:]]+ <$FUNC>/" | 
 tail -n +2 |
-awk '{printf "0x%s, 0x%s, 0x%s, 0x%s\n", $2,$3,$4,$5}'
-printf "\n"
-objdump -d $PROG |
-awk -v RS= "/^[[:xdigit:]]+ <sh>/" | 
-tail -n +2 |
-awk '{printf "0x%s, 0x%s, 0x%s, 0x%s\n", $2,$3,$4,$5}'
+awk '{printf "\"\\x%s\\x%s\\x%s\\x%s\" // %s%5s %s\n", $2,$3,$4,$5,$6,$7,$8}'
