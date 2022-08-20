916174cad73add8a0904b5fcf0824fd46d3b3bc3
pducos <pducos@student.42.fr>
2022-08-20 03:52:21 +0200

    improved script

--- script.sh
+++ script.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-FUNCTION=$2
 PROGRAM=$1
+FUNCTION=$2
 
-if [ $# -lt 1 ]; then
-    >&2 echo "Error: Usage"
-	>&2 echo "Usage: bash ./script.sh <program> [function]"
+if ! command -v otool > /dev/null; then
+    >&2 echo "Error: otool not found"
     exit -1
 fi
-
+if ! command -v objdump > /dev/null; then
+    >&2 echo "Error: objdump not found"
+    exit -1
+fi
+if [ $# -lt 1 ]; then
+	>&2 echo "Error: Usage"
+	>&2 echo "Usage: bash ./script.sh <program> [function]"
+	exit -1
+fi
 if test -f "$PROGRAM"; then
 	if [ $# -lt 2 ]; then
 		(
-			nm -Am $PROGRAM &&
-			objdump -d $PROGRAM &&
-			objdump -s $PROGRAM
+			(
+				otool -x $PROGRAM | tail -n +3 |
+				awk '{printf "0x%x: 0x%8s 0x%8s 0x%8s 0x%8s\n", $1,$2,$3,$4,$5}'
+			) && otool -xvX $PROGRAM
 		) 2> /dev/null || 
 			(>&2 printf "Error: %s: Failed to dump\n" $PROGRAM && exit 1)
 		exit 0
@@ -24,13 +32,12 @@ if test -f "$PROGRAM"; then
 		(
 			objdump -d $PROGRAM |
 			awk -v RS= "/^[[:xdigit:]]+ <$FUNCTION>/" | tail -n +2 |
-			awk '{printf "\t0x%s, 0x%s, 0x%s, 0x%s,\n", $2,$3,$4,$5,$6,$7,$8,$9}'
+			awk '{printf "\t0x%s, 0x%s, 0x%s, 0x%s,\n", $2,$3,$4,$5}'
 		) 2> /dev/null
-	) | tee >(SIZE=$(wc -w); echo "Shellcode size:" $SIZE)
+	) | tee >(SIZE=$(wc -w); echo "// Shellcode size:" $SIZE)
 
 	echo "};"
 	exit 0;
 fi
-
 >&2 printf "Error: %s: No such file\n" $PROGRAM
 exit 127
